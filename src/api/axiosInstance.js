bfe193504be9d1c4bee9f7371709164f7f2e83af
Sagor1998-debug <monjurulkabir941@gmail.com>
2025-11-13 22:02:31 +0600

    seven commit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 
 // Create Axios instance pointing to your Render backend
 const axiosInstance = axios.create({
-  baseURL: "https://rentwheels-server-3.onrender.com/api", // <-- Render backend
+  baseURL: "https://rentwheels-server-2.onrender.com/api", // <-- Render backend
 });
 
 // Interceptor to attach Firebase ID token to requests
